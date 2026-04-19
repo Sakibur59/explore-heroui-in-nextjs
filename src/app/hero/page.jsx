6bef25dc4532353410fee5a933b63319dcafd686
Sakibur59 <sakiburrahman5978@gmail.com>
2026-04-19 21:23:34 +0600

    explore hero ui in next js

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@heroui/react";
+
+
+const HeroPage = () => {
+  return (
+    <div>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="danger">Danger</Button>
+    </div>
+  );
+};
+
+export default HeroPage;
