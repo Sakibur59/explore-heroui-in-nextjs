c9176199cfff3fc6a90a31064fe22c8b1296658f
Sakibur59 <sakiburrahman5978@gmail.com>
2026-04-20 23:36:02 +0600

    add a new task in UI

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,5 +1,16 @@
+import { revalidatePath } from "next/cache";
+import { postTask } from "./task";
+
 export const createATask = async(formData) => {
     'use server'
-    const name= formData.get('name');
+    // const name= formData.get('name');
+    const newTask = Object.fromEntries(formData.entries())
     console.log("Adding a task name",formData);
+
+    const res=await postTask(newTask);
+
+    if(res.ok){
+        revalidatePath('/tasks');
+    }
+    return res;
 }
--- src/lib/task.js
+++ src/lib/task.js
@@ -3,3 +3,10 @@ import tasks from '../data/tasks.json'
 export const getTask = async() => {
     return tasks
 }
+
+export const postTask = async(newTask) => {
+    newTask.id = tasks.length+1;
+    // tasks.unshift(newTask); for add in first
+    tasks.push(newTask); //for add in last
+    return {ok:true,message:"Task Added Successfully"}
+}
